504e9dfe8d734708d9b1e27c234d24af6da41e17
Mahmoud Ali <muddibr@gmail.com>
2016-10-23 20:52:42 -0200

    Custom Renderer implemented

---
+++ Droid/MyCustomViewRenderer.cs
@@ -0,0 +1,31 @@
+﻿using Android.Widget;
+using CustomRenderers;
+using CustomRenderers.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MyCustomView), typeof(MyCustomViewRenderer))]
+namespace CustomRenderers.Droid
+{
+    public class MyCustomViewRenderer : ViewRenderer<MyCustomView, TextView>
+    {
+        public MyCustomViewRenderer()
+        {
+            
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<MyCustomView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                SetNativeControl(new TextView(Context)
+                {
+                    Text = "Oi Android"
+                });
+            }
+        }
+    }
+}
+
---
+++ iOS/MyCustomViewRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CustomRenderers;
+using CustomRenderers.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MyCustomView), typeof(MyCustomViewRenderer))]
+namespace CustomRenderers.iOS
+{
+    public class MyCustomViewRenderer : ViewRenderer<MyCustomView, UILabel>
+    {
+        public MyCustomViewRenderer()
+        {
+            
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<MyCustomView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                SetNativeControl(new UILabel()
+                {
+                    Text = "Oi iOS"
+                });
+            }
+        }
+    }
+}
+
