662120630a683309918fbb1dbb02a259f44bb872
Mahmoud Ali <mahmoud.ali@lambda3.com.br>
2016-10-24 21:24:29 -0200

    Android event implementation

---
+++ Droid/MyCustomButtonRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CustomRenderers;
+using CustomRenderers.Android;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using AButton = Android.Widget.Button;
+
+[assembly: ExportRenderer(typeof(MyCustomButton), typeof(MyCustomButtonRenderer))]
+namespace CustomRenderers.Android
+{
+    public class MyCustomButtonRenderer : ViewRenderer<MyCustomButton, AButton>
+    {
+        public MyCustomButtonRenderer()
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<MyCustomButton> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                var androidButton = new AButton(Context);
+                androidButton.Text = "Botão Custom Renderer";
+                SetNativeControl(androidButton);
+            }
+
+            if (e.OldElement != null)
+            {
+                Control.Click -= Control_Click;
+            }
+
+            if (e.NewElement != null)
+            {
+                Control.Click += Control_Click;
+            }
+        }
+
+        void Control_Click(object sender, EventArgs e)
+        {
+            ((IClickable)Element)?.Click();
+        }
+    }
+}
